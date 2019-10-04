be6daa6287aa1ef8511cc6459b62a87b538b0f91
Mental Arena <caperaven@icloud.com>
2019-10-04 18:37:41 +0200

    adding cache of expressions on compiler

---
+++ src/observer.js
@@ -0,0 +1,54 @@
+const PROXY = "_isProxy";
+const BACKUP = "__backup";
+
+export function observe(obj) {
+    Reflect.set(obj, PROXY, true);
+    Reflect.set(obj, BACKUP, {});
+
+    const proxy = new Proxy(obj, {
+        get: get,
+        set: set
+    });
+
+    return proxy;
+}
+
+export function releaseObserved(obj) {
+    if (obj.dispose != null) {
+        obj._disposing = true;
+        obj.dispose();
+    }
+
+    delete obj[PROXY];
+    delete obj[BACKUP];
+}
+
+function get(obj, prop) {
+    return Reflect.get(obj, prop);
+}
+
+function set(obj, prop, value) {
+    if (obj._disposing == true) return false;
+
+    const backup = Reflect.get(obj, BACKUP);
+    const oldValue = Reflect.get(obj, prop);
+
+    Reflect.set(obj, prop, value);
+
+    if (obj.propertyChanged != null) {
+        obj.propertyChanged(value, oldValue);
+    }
+
+    if (isProxy(oldValue)) {
+        releaseObserved(oldValue);
+    }
+    else {
+        Reflect.set(backup, prop, oldValue);
+    }
+
+    return true;
+}
+
+function isProxy(obj) {
+    return obj && typeof obj == "object" && Reflect.get(obj, PROXY) == true;
+}
---
+++ test/observer.test.js
@@ -0,0 +1,68 @@
+import {observe} from "./../src/observer.js"
+
+test("observer", () => {
+    const px = observe({
+        name: "John",
+        lastName: "Doe",
+        propertyChanged: jest.fn()
+    });
+
+    expect(px._isProxy).toBe(true);
+    expect(px.__backup).not.toBeNull();
+});
+
+test("observer - propertyChagned: value", () => {
+    const px = observe({
+        name: "John",
+        lastName: "Doe",
+        propertyChanged: jest.fn()
+    });
+});
+
+test("observer - propertyChanged: complex object", () => {
+    const px = observe({
+        name: "John",
+        lastName: "Doe",
+        propertyChanged: jest.fn()
+    });
+
+    px.name = "John2";
+    expect(px.propertyChanged).toHaveBeenCalled();
+    expect(px.name).toBe("John2");
+    expect(px.__backup.name).toBe("John");
+});
+
+test ("observer - subObjects", () => {
+    const address = {
+        street: "Street 1",
+        dispose: jest.fn()
+    };
+
+    const oldStreets = [];
+
+    const px = observe({
+        address: observe(address),
+        propertyChanged: (newValue, oldValue) => {
+            if (newValue.street == "street 2") {
+                expect(oldValue.street).toBe("Street 1");
+            }
+            else if (newValue.street == "street 3") {
+                expect(oldValue.street).toBe("Street 2")
+            }
+            oldStreets.push(oldValue.street);
+        }
+    });
+
+    px.address = observe({
+       street: "Street 2"
+    });
+
+    px.address = observe({
+        street: "Street 3"
+    });
+
+    expect(address.dispose).toHaveBeenCalled();
+    expect(oldStreets.length).toBe(2);
+    expect(oldStreets[0]).toBe("Street 1");
+    expect(oldStreets[1]).toBe("Street 2");
+});
