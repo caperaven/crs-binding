a8570f863ec18bc5efec244b939ede1278b9c168
Mental Arena <caperaven@icloud.com>
2019-10-09 17:40:42 +0200

    added two way binding

--- src/binding/providers/bind-provider.js
+++ src/binding/providers/bind-provider.js
@@ -1,5 +1,35 @@
 import {OneWayProvider} from "./one-way-provider.js";
 
 export class BindProvider extends OneWayProvider {
+    dispose() {
+        this._element.removeEventListener("change", this._changeHandler);
+        this._changeHandler = null;
+        super.dispose();
+    }
 
+    initialize() {
+        super.initialize();
+        this._changeHandler = this._change.bind(this);
+        this._element.addEventListener("change", this._changeHandler);
+    }
+
+    _change(event) {
+        let value = event.target.value;
+        const type = event.target.type || "text";
+        const typeFn = `_${type}`;
+
+        if (this[typeFn] != null) {
+            value = this[typeFn](value);
+        }
+
+        this._context[this._value] = value;
+    }
+
+    _number(value) {
+        return Number(value);
+    }
+
+    _date(value) {
+        return new Date(value);
+    }
 }
