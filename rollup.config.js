1a6c255e877df714308af3d9f26fff34632894ff
Mental Arena <caperaven@icloud.com>
2019-12-10 00:22:24 +0200

    fixed issue with updates on bindable element

--- rollup.config.js
+++ rollup.config.js
@@ -4,37 +4,37 @@ export default [
     {
         input: "src/index.js",
         output: [
-            {file: 'dist/crs-binding.js', format: 'es', sourcemap: true}
+            {file: 'dist/crs-binding.js', format: 'es', sourcemap: false}
         ],
         plugins: [
-            //terser()
+            terser()
         ]
     },
     {
         input: "src/events/event-mixin.js",
         output: [
-            {file: 'dist/crs-event-mixin.js', format: 'es', sourcemap: true}
+            {file: 'dist/crs-event-mixin.js', format: 'es', sourcemap: false}
         ],
         plugins: [
-            //terser()
+            terser()
         ]
     },
     {
         input: "src/binding/bindable-element.js",
         output: [
-            {file: 'dist/crs-bindable-element.js', format: 'es', sourcemap: true}
+            {file: 'dist/crs-bindable-element.js', format: 'es', sourcemap: false}
         ],
         plugins: [
-            //terser()
+            terser()
         ]
     },
     {
         input: "src/view/view-base",
         output: [
-            {file: 'dist/crs-view-base.js', format: 'es', sourcemap: true}
+            {file: 'dist/crs-view-base.js', format: 'es', sourcemap: false}
         ],
         plugins: [
-            //terser()
+            terser()
         ]
     }
 ];
