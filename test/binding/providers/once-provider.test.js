e52b80e8c979905f8b152a925c13c4e083241a7d
Mental Arena <caperaven@icloud.com>
2020-02-10 11:46:23 +0200

    update tests

--- test/binding/providers/once-provider.test.js
+++ test/binding/providers/once-provider.test.js
@@ -1,6 +1,11 @@
 import {OnceProvider} from "./../../../src/binding/providers/once-provider.js"
 import {ElementMock} from "../../element.mock.js";
 
+beforeEach(async () => {
+    const bindingModule = await import("./../../crsbinding.mock.js");
+    global.crsbinding = bindingModule.crsbinding;
+})
+
 test("Once provider - execute", () => {
     const element = new ElementMock();
 
