2074b33cca2300205c359ae68a16ef0e6901c76d
Mental Arena <caperaven@icloud.com>
2020-02-11 21:41:33 +0200

    path test

---
+++ app/form/form.js
@@ -0,0 +1,24 @@
+import {ViewBase} from "./../../src/view/view-base.js";
+
+export default class Form extends ViewBase {
+    async connectedCallback() {
+        super.connectedCallback();
+        this.title = "Personal Input Form";
+        this.data = getData();
+    }
+
+    disposeData() {
+        //JHR: TODO: NB: DEBUG THIS AND MAKE SURE EVERYTHING GETS DESTROYED
+        this.data = null;
+    }
+}
+
+function getData() {
+    return {
+        personal: {
+            firstName: "John",
+            lastName: "Doe",
+            age: 21
+        }
+    }
+}
