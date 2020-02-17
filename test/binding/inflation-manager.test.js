9df1aefcdcfe88b11f78a821c11c6f303a369fec
caperaven <caperaven@gmail.com>
2020-02-17 12:33:10 +0200

    added class list tests

--- test/binding/inflation-manager.test.js
+++ test/binding/inflation-manager.test.js
@@ -3,17 +3,31 @@ import {ElementMock} from "../element.mock.js";
 
 let instance;
 let template;
+let model;
 
 async function createTemplate() {
     const bindingModule = await import("./../crsbinding.mock.js");
     global.crsbinding = bindingModule.crsbinding;
 
     template = new ElementMock("template");
-
     const li = new ElementMock("li", "listitem");
-    li.setAttribute("data-id", "${id}");
+
+    const child1 = new ElementMock("div");
+    child1.setAttribute("data-id", "${id}");
+    li.appendChild(child1);
+
+    const child2 = new ElementMock("div");
+    child2.setAttribute("classlist.if", "isReady == true ? 'ready' : 'notReady'");
+    li.appendChild(child2);
 
     template.appendChild(li);
+
+    instance.register("list-item", template);
+
+    model = {
+        id: 10,
+        isReady: true
+    }
 }
 
 beforeEach(async () => {
@@ -31,18 +45,28 @@ test("inflation manger - dispose", () => {
 });
 
 test("inflation manager - register", () => {
-    instance.register("list-item", template);
     const gen = instance._items.get("list-item");
 
     expect(gen).not.toBeUndefined();
     expect(gen.template).not.toBeUndefined();
     expect(gen.inflate).not.toBeUndefined();
     expect(gen.deflate).not.toBeUndefined();
+});
 
+test("inflation manager - check attribute value", () => {
     const el = template.children[0];
-    instance.inflate("list-item", el, {id: 10});
-    expect(el.getAttribute("data-id").value).toEqual(10);
+    instance.inflate("list-item", el, model);
+    expect(el.children[0].getAttribute("data-id").value).toEqual(model.id);
 
     instance.deflate("list-item", el);
-    expect(el.getAttribute("data-id")).toBeUndefined();
+    expect(el.children[0].getAttribute("data-id")).toBeUndefined();
+});
+
+test("inflation manager - check classlist", () => {
+    const el = template.children[0];
+    instance.inflate("list-item", el, model);
+    expect(el.children[1].classList.contains("ready")).toBeTruthy();
+
+    instance.deflate("list-item", el);
+    expect(el.children[1].classList.length).toBe(0);
 });
--- test/element.mock.js
+++ test/element.mock.js
@@ -74,16 +74,36 @@ export class ElementMock {
 }
 
 class ClassList {
+    get length() {
+        return this.items.length;
+    }
+
     constructor() {
         this.items = [];
     }
 
     add(item) {
+        if (Array.isArray(item)) {
+            for (let i of item) {
+                this.items.push(i);
+            }
+        }
         this.items.push(item);
     }
 
     contains(item) {
         const result = this.items.find(cls => cls == item);
         return result != null;
+    }
+
+    remove(item) {
+        const index = this.items.indexOf(item);
+        if (index != -1) {
+            this.items.splice(index, 1);
+        }
+    }
+
+    item(index) {
+        return this.items[index];
     }
 }
