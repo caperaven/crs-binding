0096e89dc43082bcbeafcdedcd34e35b600f37a0
caperaven <caperaven@gmail.com>
2020-02-27 13:02:21 +0200

    added tests

---
+++ test/lib/clone.test.js
@@ -0,0 +1,34 @@
+import {clone} from "./../../src/lib/clone.js";
+
+beforeAll(async () => {
+    const bindingModule = await import("./../crsbinding.mock.js");
+    global.crsbinding = bindingModule.crsbinding;
+});
+
+test("clone", () => {
+    const source = {test: "testing"};
+    const result = crsbinding.utils.clone(source);
+    expect(result.test).toBe(source.test);
+    expect(result).not.toBe(source);
+});
+
+test("clone - observed", () => {
+    const source = crsbinding.observation.observe({test: "testing"});
+    const eventSource = {__events: new Map([["key", "value"]])};
+    const result = crsbinding.utils.clone(source, eventSource);
+    expect(result.test).toBe(source.test);
+    expect(result).not.toBe(source);
+});
+
+test("clone - observed", () => {
+    const source = crsbinding.observation.observe({test: "testing"});
+    source.site = crsbinding.observation.observe({code: "A11"});
+
+    const eventSource = {__events: new Map([["key", "value"]])};
+    const result = crsbinding.utils.clone(source, eventSource);
+
+    expect(result.test).toBe(source.test);
+    expect(result).not.toBe(source);
+    expect(result.site.code).toBe(source.site.code);
+    expect(result.site).not.toBe(source.site);
+});
