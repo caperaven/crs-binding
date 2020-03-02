c02dacc6fe892bbef67a13eb7b95fc10ce1996ce
Johan Rabie <johan.rabie@pragmaworld.net>
2020-03-02 20:09:34 +0200

    added the notifyPropertyOn

--- app/calc/calc.js
+++ app/calc/calc.js
@@ -2,16 +2,32 @@ import {ViewBase} from "./../../src/view/view-base.js";
 
 export default class Calc extends ViewBase {
     get duration() {
-        return "00:10";
+        const startS = this._durToSec(this.start);
+        const endS = this._durToSec(this.end);;
+
+        const result = endS - startS;
+        return this._secToDur(result);
     }
     
     async connectedCallback() {
-        super.connectedCallback();
         this.title = "Personal Input Form";
-        
         this.start = "01:00";
         this.end = "01:20";
-        //crsbinding.events.notifyPropertyChanged(this, "duration");
+
+        super.connectedCallback();
         crsbinding.events.notifyPropertyOn(this, "duration", ["start", "end"]);
+    }
+
+    _durToSec(value) {
+        const parts = value.split(":");
+        const hours = Number.parseInt(parts[0].trim());
+        const min = Number.parseInt(parts[1].trim());
+        return min + hours * 60;
+    }
+
+    _secToDur(value) {
+        const hours = value < 60 ? 0 : value / 60;
+        const min = value - hours;
+        return `${hours < 10 ? "0" : ""}${hours}:${min < 10 ? "0": ""}${min}`;
     }
 }   
