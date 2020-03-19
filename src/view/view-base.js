33c0146a14d6504548ea8aa8a1cd1a206ba552d9
caperaven <caperaven@gmail.com>
2020-03-19 23:48:19 +0200

    take out display none of view base

--- src/view/view-base.js
+++ src/view/view-base.js
@@ -51,6 +51,6 @@ export class ViewBase {
 
     _loaded() {
         crsbinding.expression.updateUI(this);
-        this.element.style.visibility = "";
+        this._element.style.visibility = "";
     }
 }
