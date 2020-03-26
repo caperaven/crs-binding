95a1325571b4c8e35a9cd3b9064cf0866228e3fa
caperaven <caperaven@gmail.com>
2020-03-26 19:14:30 +0200

    added clone test

--- src/index.js
+++ src/index.js
@@ -29,8 +29,6 @@ function capitalizePropertyPath(str) {
 function disposeProperties(obj) {
     const properties = Object.getOwnPropertyNames(obj).filter(prop => obj[prop] && obj[prop].__isProxy == true);
     for (let property of properties) {
-        console.log(property);
-
         const pObj = obj[property];
         if (Array.isArray(pObj) != true) {
             disposeProperties(pObj);
