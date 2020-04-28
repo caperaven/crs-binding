91395fef42bf726844021e03ee8f30457e76aa50
caperaven <caperaven@gmail.com>
2020-04-28 16:56:31 +0200

    remove example files

---
+++ app/component/component.js
@@ -0,0 +1,11 @@
+import {ViewBase} from "../../src/view/view-base.js";
+
+export default class Component extends ViewBase {
+    async connectedCallback() {
+        super.connectedCallback();
+    }
+
+    async disconnectedCallback() {
+        super.disconnectedCallback();
+    }
+}
--- app/svg/svg.js
+++
@@ -1,9 +0,0 @@
-import {ViewBase} from "./../../src/view/view-base.js";
-
-export default class Svg extends ViewBase {
-    async connectedCallback() {
-        this.title = "SVG Test";
-        this.isActive = true;
-        super.connectedCallback();
-    }
-}
