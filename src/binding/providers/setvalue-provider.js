99529ec3acd9d142c0f7dab6dc782043e8f2fda2
caperaven <caperaven@gmail.com>
2020-06-11 15:32:51 +0200

    get first sample of setValue provider working

--- src/binding/providers/setvalue-provider.js
+++ src/binding/providers/setvalue-provider.js
@@ -18,10 +18,17 @@ export class SetValueProvider extends CallProvider {
         if (part.indexOf("$globals") != -1) {
             return this._getGlobalSetter(part, value);
         }
+        else {
+            return this._getContextSetter(part, value);
+        }
     }
 
     _getGlobalSetter(part, value) {
         const path = part.replace("$globals.", "");
         return `crsbinding.data.setProperty(crsbinding.$globals, "${path}", ${value});`;
+    }
+
+    _getContextSetter(part, value) {
+        return `crsbinding.data.setProperty(${this._context}, "${part}", ${value});`;
     }
 }
