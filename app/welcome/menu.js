1b8ac1c89bfa78ea858db4b7f9b0d5a4dbd98b2e
caperaven <caperaven@gmail.com>
2020-06-12 08:22:08 +0200

    removed redundent component for welcome

--- app/welcome/menu.js
+++
@@ -1,9 +0,0 @@
-import {BindableElement} from "../../src/binding/bindable-element.js";
-
-export class Menu extends BindableElement {
-    get html() {
-        return import.meta.url.replace(".js", ".html");
-    }
-}
-
-customElements.define("main-menu", Menu);
--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -1,5 +1,4 @@
 import {ViewBase} from "../../src/view/view-base.js";
-import "./menu.js";
 
 export default class Welcome extends ViewBase {
 
