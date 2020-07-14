d1819651b333ec16a3be67ba593c3f1d0ed7a4f2
caperaven <caperaven@gmail.com>
2020-07-14 18:03:27 +0200

    memory leak example added

---
+++ app/binding1/binding1.js
@@ -0,0 +1,26 @@
+import {ViewBase} from "../../src/view/view-base.js";
+
+export default class Binding1 extends ViewBase {
+    async preLoad(setPropertyCallback) {
+        setPropertyCallback("items", [
+            { title: "Item 1", isDone: false },
+            { title: "Item 2", isDone: false }
+        ]);
+    }
+
+    load() {
+        crsbinding.data.updateUI(this, "items");
+        super.load();
+    }
+
+    addItem() {
+        const items = crsbinding.data.array(this, "items");
+        const title = prompt("Title", `Item ${items.length + 1}`);
+        items.push({title: title, isDone: false});
+    }
+
+    removeSelected() {
+        const items = crsbinding.data.getValue(this, "items").filter(item => item.isSelected != true);
+        crsbinding.data.setProperty(this, "items", items);
+    }
+}
