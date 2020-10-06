254212f806a8afa6ec485e053ccb60a4cf35c0ab
caperaven <caperaven@gmail.com>
2020-10-06 14:28:23 +0200

    add test for svg for binding

---
+++ app/svg-binding/svg-binding.js
@@ -0,0 +1,59 @@
+export default class SvgBinding extends crsbinding.classes.ViewBase {
+    preLoad() {
+        this.createItems();
+    }
+
+    load() {
+        crsbinding.data.updateUI(this, "items");
+        super.load();
+    }
+
+    createItems() {
+        const items = [];
+        let x = 20;
+
+        for (let i = 0; i < 1000; i++) {
+            items.push({
+                x: x,
+                y: 10,
+                r: 5
+            })
+
+            x += 20;
+        }
+
+        this.setProperty("items", items);
+    }
+
+    update() {
+        const array = this.getProperty("items");
+        for (let item of array) {
+            item.y = this.getRndInteger(10, 100);
+            item.r = this.getRndInteger(5, 10);
+            crsbinding.data.updateUI(item);
+        }
+    }
+
+    startAnimate() {
+        const array = this.getProperty("items");
+
+        this.interval = setInterval(() => {
+            for (let item of array) {
+                item.y = this.getRndInteger(10, 200);
+                item.r = this.getRndInteger(5, 10);
+
+                if (item.x < 800) {
+                    crsbinding.data.updateUI(item);
+                }
+            }
+        }, 1000)
+    }
+
+    stopAnimate() {
+        clearInterval(this.interval);
+    }
+
+    getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
+}
