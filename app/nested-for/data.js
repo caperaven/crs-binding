375f87c0a4890dd88a61c57010fd7509cd53d6c6
caperaven <caperaven@gmail.com>
2020-11-17 15:52:21 +0200

    add nexted for example

---
+++ app/nested-for/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        title: "Item 1",
+        items: [
+            {
+                title: "Item 1 - Child 1"
+            },
+            {
+                title: "Item 1 - Child 2"
+            },
+            {
+                title: "Item 1 - Child 3"
+            }
+        ]
+    },
+    {
+        title: "Item 2",
+        items: [
+            {
+                title: "Item 2 - Child 1"
+            },
+            {
+                title: "Item 2 - Child 2"
+            },
+            {
+                title: "Item 2 - Child 3"
+            }
+        ]
+    },
+    {
+        title: "Item 3",
+        items: [
+            {
+                title: "Item 3 - Child 1"
+            },
+            {
+                title: "Item 3 - Child 2"
+            },
+            {
+                title: "Item 3 - Child 3"
+            }
+        ]
+    }
+]
