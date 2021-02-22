0b8bf6a011a8c2c842635271d660d87ddd1820d1
caperaven <caperaven@gmail.com>
2021-02-22 17:28:11 +0200

    add tokenizer

---
+++ src/expressions/exp-tokenizer.js
@@ -0,0 +1,143 @@
+export function tokenize(exp) {
+    const result = [];
+    let word = [];
+    let i = 0;
+
+    function step(type, value) {
+        if (word.length > 0) {
+            const value = word.join("");
+            let wordType = "word"
+
+            if (keywords.indexOf(value) != -1) {
+                wordType = "keyword";
+            }
+
+            if (isNaN(Number(value)) == false) {
+                wordType = "number";
+            }
+
+            result.push({type: wordType, value: value});
+            word.length = 0;
+        }
+
+        result.push({type: type, value: value});
+    }
+
+    for (i; i < exp.length; i++) {
+        const char = exp[i];
+
+        if (char == " ") {
+            step("space", " ");
+            continue;
+        }
+
+        if (char == "`") {
+            step("literal", "`null`");
+            continue;
+        }
+
+        // check for string literal variable markers
+        if (char == "$") {
+            if (exp[i + 1] == "{") {
+                step("keyword", "${");
+                i++;
+                continue;
+            }
+        }
+
+        // check for hardcoded string values
+        if (char == "'" || char == '"') {
+            const lastIndex = i + 1 + exp.length - i;
+            let j = i + 1;
+            for (j; j < lastIndex; j++) {
+                if (exp[j] == char) {
+                    const value = exp.substring(i, j + 1);
+                    step("string", value);
+                }
+            }
+            i = j + 1;
+            continue;
+        }
+
+        // check for single character key words
+        if (keywords.indexOf(char) != -1) {
+            step("keyword", char);
+            continue;
+        }
+
+        // check for operators
+        if (operatorStart.indexOf(char) != -1) {
+            for (let j = i; j < i + 4; j++) {
+                const charNext = exp[j];
+                if (operatorStart.indexOf(charNext) == -1) {
+                    const value = exp.substring(i, j);
+                    step("operator", value);
+                    i = j - 1;
+                    break;
+                }
+            }
+            continue;
+        }
+
+        word.push(char);
+    }
+
+    if (word.length > 0) {
+        result.push({type: "word", value: word.join("")});
+    }
+
+    return postProcessTokens(result);
+}
+
+function postProcessTokens(tokens) {
+    if (tokens.length == 1 && tokens[0].type == "word") {
+        tokens[0].type = "property";
+        return tokens;
+    }
+
+    let state = null;
+
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        if (token.type == "word") {
+            // word is inside a ${...} expression so must be a property
+            if (state == "literal") {
+                token.type = "property";
+            }
+
+            // word contains "." indicating a path expression
+            else if (token.value.indexOf(".") != -1) {
+                token.type = "property";
+            }
+
+            // left of operator
+            else if (isOperator(tokens[i + 1]) || isOperator(tokens[i + 2])) {
+                token.type = "property";
+            }
+
+            // right of operator
+            else if (isOperator(tokens[i - 1]) || isOperator(tokens[i - 2])) {
+                token.type = "property";
+            }
+        }
+
+        // manage current state for processing
+        if (token.type == "keyword" && "${") {
+            state = "literal";
+        }
+
+        if (state == "literal" && token.value == "}") {
+            state = null;
+        }
+    }
+
+    return tokens;
+}
+
+function isOperator(token) {
+    if (token == null) return false;
+    return token.type == "operator";
+}
+
+const operatorStart = ["=", "!", "<", ">", "+", "-", "*", "/", "&", "|"];
+const keywords = ["{", "}", "(", ")", "true", "false", "null", "undefined"];
---
+++ test/events/exp-tokenizer.test.js
@@ -0,0 +1,398 @@
+import {tokenize} from "../../src/expressions/exp-tokenizer.js"
+
+test("tokenize - single world", () => {
+    const result = tokenize("color");
+    expect(result.length).toBe(1);
+    expect(result[0].type).toBe("property");
+    expect(result[0].value).toBe("color");
+})
+
+test("tokenize - single world string literal", () => {
+    const result = tokenize("${color}");
+    expect(result.length).toBe(3);
+
+    expect(result[0].type).toBe("keyword");
+    expect(result[0].value).toBe("${");
+
+    expect(result[1].type).toBe("property");
+    expect(result[1].value).toBe("color");
+
+    expect(result[2].type).toBe("keyword");
+    expect(result[2].value).toBe("}");
+})
+
+test("tokenize - simple expression", () => {
+    const result = tokenize("property1 == 'a'");
+    expect(result.length).toBe(5);
+
+    expect(result[0].type).toBe("property");
+    expect(result[0].value).toBe("property1");
+
+    expect(result[1].type).toBe("space");
+
+    expect(result[2].type).toBe("operator");
+    expect(result[2].value).toBe("==");
+
+    expect(result[3].type).toBe("space");
+
+    expect(result[4].type).toBe("string");
+    expect(result[4].value).toBe("'a'");
+})
+
+test("tokenize - simple string literal expression", () => {
+    const result = tokenize("property1 == `${value1}`");
+    expect(result.length).toBe(9);
+
+    expect(result[0].type).toBe("property");
+    expect(result[0].value).toBe("property1");
+
+    expect(result[1].type).toBe("space");
+
+    expect(result[2].type).toBe("operator");
+    expect(result[2].value).toBe("==");
+
+    expect(result[3].type).toBe("space");
+
+    expect(result[4].type).toBe("literal");
+
+    expect(result[5].type).toBe("keyword");
+    expect(result[5].value).toBe("${");
+
+    expect(result[6].type).toBe("property");
+    expect(result[6].value).toBe("value1");
+
+    expect(result[7].type).toBe("keyword");
+    expect(result[7].value).toBe("}");
+
+    expect(result[8].type).toBe("literal");
+})
+
+test("tokenize - simple property with function", () => {
+    const result = tokenize("property1.toUpper() == 'A'");
+    expect(result.length).toBe(7);
+
+    expect(result[0].type).toBe("property");
+    expect(result[0].value).toBe("property1.toUpper");
+    expect(result[1].type).toBe("keyword");
+    expect(result[1].value).toBe("(");
+    expect(result[2].type).toBe("keyword");
+    expect(result[2].value).toBe(")");
+    expect(result[3].type).toBe("space");
+    expect(result[4].type).toBe("operator");
+    expect(result[4].value).toBe("==");
+    expect(result[5].type).toBe("space");
+    expect(result[6].type).toBe("string");
+    expect(result[6].value).toBe("'A'");
+})
+
+test("tokenize - simple composite expression", () => {
+    const result = tokenize("${model.name} is ${model.age} old");
+    expect(result.length).toBe(11);
+
+    expect(result[0].type).toBe("keyword");
+    expect(result[0].value).toBe("${");
+
+    expect(result[1].type).toBe("property");
+    expect(result[1].value).toBe("model.name");
+
+    expect(result[2].type).toBe("keyword");
+    expect(result[2].value).toBe("}");
+
+    expect(result[3].type).toBe("space");
+
+    expect(result[4].type).toBe("word");
+    expect(result[4].value).toBe("is");
+
+    expect(result[5].type).toBe("space");
+
+    expect(result[6].type).toBe("keyword");
+    expect(result[6].value).toBe("${");
+
+    expect(result[7].type).toBe("property");
+    expect(result[7].value).toBe("model.age");
+
+    expect(result[8].type).toBe("keyword");
+    expect(result[8].value).toBe("}");
+
+    expect(result[9].type).toBe("space");
+
+    expect(result[10].type).toBe("word");
+    expect(result[10].value).toBe("old");
+})
+
+
+/*
+
+test("sanitizeExp - composite", () => {
+    const result = sanitizeExp("${model.name} is ${model.age} old");
+    expect(result.properties[0]).toBe("model.name");
+    expect(result.properties[1]).toBe("model.age");
+    expect(result.expression).toBe("${context.model.name} is ${context.model.age} old");
+    expect(result.isLiteral).toBe(true);
+});
+
+test("sanitizeExp - expression", () => {
+    const result = sanitizeExp("${1 + 1 + property1}");
+    expect(result.properties[0]).toBe("property1");
+    expect(result.expression).toBe("${1 + 1 + context.property1}");
+    expect(result.isLiteral).toBe(true);
+});
+
+test("sanitizeExp - multiple functions", () => {
+    const result = sanitizeExp("${firstName.trim().toLowerCase()} > ${lastName.trim().toLowerCase()}");
+    expect(result.expression).toBe("${context.firstName.trim().toLowerCase()} > ${context.lastName.trim().toLowerCase()}");
+    expect(result.properties[0]).toBe("firstName");
+    expect(result.properties[1]).toBe("lastName");
+    expect(result.isLiteral).toBe(true);
+});
+
+test("sanitizeExp - conditional value", () => {
+    const result = sanitizeExp("title == 'a' ? true : false");
+    expect(result.expression).toBe("context.title == 'a' ? true : false");
+    expect(result.properties.length).toBe(1);
+    expect(result.properties[0]).toBe("title");
+});
+
+test ("sanitizeExp - path", () => {
+    const result = sanitizeExp("address.street");
+    expect(result.expression).toBe("context.address.street");
+    expect(result.properties[0]).toBe("address.street");
+});
+
+test ("sanitizeExp - special string", () => {
+    const result = sanitizeExp("value == 'v-height'");
+    expect(result.expression).toBe("context.value == 'v-height'");
+});
+
+test ("sanitizeExp - when", () => {
+    const result = sanitizeExp("firstName == 'John' && lastName == 'Doe'");
+    expect(result.expression).toBe("context.firstName == 'John' && context.lastName == 'Doe'");
+    expect(result.properties[0]).toBe("firstName");
+    expect(result.properties[1]).toBe("lastName");
+});
+
+test ("sanitizeExp - string token", () => {
+    const result = sanitizeExp("${firstName} ${lastName} is ${age} old and lives at \"${address.street}\"");
+    expect(result.expression).toBe("${context.firstName} ${context.lastName} is ${context.age} old and lives at \"${context.address.street}\"")
+    expect(result.properties[0]).toBe("firstName");
+    expect(result.properties[1]).toBe("lastName");
+    expect(result.properties[2]).toBe("age");
+    expect(result.properties[3]).toBe("address.street");
+});
+
+test("sanitizeExp - ignore named expression", () => {
+    const result = sanitizeExp("person.firstName", "person");
+    expect(result.expression).toBe("person.firstName");
+    expect(result.properties[0]).toBe("person.firstName");
+});
+
+test("sanitizeExp - ignore named expression - multiple", () => {
+    const result = sanitizeExp("person.firstName && person.lastName", "person");
+    expect(result.expression).toBe("person.firstName && person.lastName");
+    expect(result.properties[0]).toBe("firstName");
+    expect(result.properties[1]).toBe("lastName");
+});
+
+test("sanitizeExp - ignore null", () => {
+    const result = sanitizeExp("validation.editing == null", "context");
+    expect(result.expression).toBe("context.validation.editing == null");
+    expect(result.properties[0]).toBe("validation.editing");
+});
+
+test("sanitizeExp - array in expression", () => {
+    const result = sanitizeExp("arrayfield != null || arrayfield.length == 0", "person");
+    expect(result.expression).toBe("person.arrayfield != null || person.arrayfield.length == 0");
+    expect(result.properties[0]).toBe("arrayfield");
+    expect(result.properties[1]).toBe("arrayfield.length");
+});
+
+test("sanitizeExp - array in expression", () => {
+    const result = sanitizeExp("arrayfield.length == 0 || arrayfield.length == 5", "person");
+    expect(result.expression).toBe("person.arrayfield.length == 0 || person.arrayfield.length == 5");
+    expect(result.properties[0]).toBe("arrayfield.length");
+    expect(result.properties.length).toBe(1);
+});
+
+test("sanitizeExp - set object", () => {
+    const result = sanitizeExp("$globals.date = {title: ${title}}");
+    expect(result.expression).toBe("crsbinding.data.globals.date = {title: ${context.title}}");
+    expect(result.properties[0]).toBe("title");
+});
+
+test("sanitizeExp - set object with event", () => {
+    const result = sanitizeExp("{ x: $event.x, y: $event.y }");
+    expect(result.expression).toBe("{ x: event.x, y: event.y }");
+    expect(result.properties[0]).toBe("x");
+    expect(result.properties[1]).toBe("y");
+});
+
+test("sanitizeExp - toggle boolean", () => {
+    const result = sanitizeExp("$context.isOpen = !$context.isOpen");
+    expect(result.expression).toBe("context.isOpen = !context.isOpen");
+});
+
+test("sanitizeExp - !$context.expression", () => {
+    const result = sanitizeExp("!$context.isOpen");
+    expect(result.expression).toBe("!context.isOpen");
+});
+
+test("sanitizeExp - !expression", () => {
+    const result = sanitizeExp("${!isOpen}");
+    expect(result.expression).toBe("${!context.isOpen}");
+});
+
+test("sanitizeExp - $event.target", () => {
+    const result = sanitizeExp("$event.target");
+    expect(result.expression).toBe("event.target");
+    expect(result.properties.length).toBe(0);
+});
+
+test("sanitizeExp - $parentId in expression", () => {
+    const result = sanitizeExp("$parent.property1 == item.property2", "item");
+    expect(result.expression).toBe("parent.property1 == item.property2");
+
+    expect(result.properties[0]).toBe("$parent.property1");
+    expect(result.properties[1]).toBe("property2");
+});
+
+test("sanitizeExp - $data", () => {
+    const result = sanitizeExp("selectedObj = $data($event.target.dataset.id)");
+    expect(result.expression).toBe("context.selectedObj = crsbinding.data.getValue(event.target.dataset.id)");
+    expect(result.properties.length).toBe(1);
+    expect(result.properties[0]).toBe("selectedObj");
+});
+
+test("sanitizeExp - inner-text", () => {
+    const result = sanitizeExp("This is the ${item.position} article", "item");
+    expect(result.expression).toBe("This is the ${item.position} article");
+    expect(result.properties[0]).toBe("position");
+});
+
+test("sanitizeExp - keywords", () => {
+    let result = sanitizeExp("true");
+    expect(result.expression).toBe("true");
+
+    result = sanitizeExp("false");
+    expect(result.expression).toBe("false");
+
+    result = sanitizeExp("null");
+    expect(result.expression).toBe("null");
+
+    result = sanitizeExp(true);
+    expect(result.expression).toBe(true);
+
+    result = sanitizeExp(false);
+    expect(result.expression).toBe(false);
+
+    result = sanitizeExp(null);
+    expect(result.expression).toBe(null);
+
+    result = sanitizeExp(10);
+    expect(result.expression).toBe(10);
+
+    result = sanitizeExp("10");
+    expect(result.expression).toBe("10");
+
+})
+
+test("sanitizeExp - expression with (....)", () => {
+    const result = sanitizeExp("model.monitoringPointTriggerExpressionId != null || (model.status == 'CancelledByUser' || model.status == 'CancelledBySystem' || model.status == 'Closed')");
+    expect(result.expression).toBe("context.model.monitoringPointTriggerExpressionId != null || (context.model.status == 'CancelledByUser' || context.model.status == 'CancelledBySystem' || context.model.status == 'Closed')");
+    expect(result.properties.length).toBe(2);
+    expect(result.properties[0]).toBe("model.monitoringPointTriggerExpressionId");
+    expect(result.properties[1]).toBe("model.status");
+});
+
+test("sanitize - expression with (...) simple combined with function", () => {
+    const result = sanitizeExp("(model.property.isValid() == true)");
+    expect(result.expression).toBe("(context.model.property.isValid() == true)");
+    expect(result.properties[0]).toBe("model.property");
+})
+
+test("sanitize - expression with (...) simple combined with function and parameters", () => {
+    const result = sanitizeExp("(model.property.isValid('abc', 10) == true)");
+    expect(result.expression).toBe("(context.model.property.isValid('abc', 10) == true)");
+    expect(result.properties[0]).toBe("model.property");
+})
+
+test("sanitize - expression with (()) simple", () => {
+    const result = sanitizeExp("(model.isOpen == true) || (model.isOpen == null)");
+    expect(result.expression).toBe("(context.model.isOpen == true) || (context.model.isOpen == null)");
+    expect(result.properties[0]).toBe("model.isOpen");
+})
+
+test("sanitize - expression with (()) complex", () => {
+    const result = sanitizeExp("((model.isOpen == true) || (model.isOpen == null))");
+    expect(result.expression).toBe("((context.model.isOpen == true) || (context.model.isOpen == null))");
+    expect(result.properties[0]).toBe("model.isOpen");
+})
+
+test("sanitize - function", () => {
+    const result = sanitizeExp("`rotate(${angle}deg)`");
+    expect(result.expression).toBe("`rotate(${context.angle}deg)`");
+    expect(result.properties[0]).toBe("angle");
+})
+
+test("sanitize - calculated string", () => {
+    const result = sanitizeExp("`${(rect.x / 2)}px ${(rect.y / 2)}px`");
+    expect(result.expression).toBe("`${(context.rect.x / 2)}px ${(context.rect.y / 2)}px`");
+    expect(result.properties[0]).toBe("rect.x");
+    expect(result.properties[1]).toBe("rect.y");
+})
+
+test("sanitize - html", () => {
+    const result = sanitizeExp("$html.model.property");
+    expect(result.isHTML).toBeTruthy();
+    expect(result.expression).toBe("context.model.property");
+})
+
+test("sanitize - expression", () => {
+    const result = sanitizeExp("${model.siteCode == 'A21' ? 'Hello A21' : model.code}");
+    expect(result.expression).toBe("${context.model.siteCode == 'A21' ? 'Hello A21' : context.model.code}");
+})
+
+test("sanitize - expression literal", () => {
+    const result = sanitizeExp("`${model.siteCode == 'A21' ? 'Hello A21' : model.code}`");
+    expect(result.expression).toBe("`${context.model.siteCode == 'A21' ? 'Hello A21' : context.model.code}`");
+})
+
+test("sanitize - Not expressions", () => {
+    const result = sanitizeExp("!isActive");
+    expect(result.expression).toBe("!context.isActive");
+})
+
+test("sanitize - Not expressions on path", () => {
+    const result = sanitizeExp("!model.isActive");
+    expect(result.expression).toBe("!context.model.isActive");
+})
+
+test("sanitize - Not expressions in literals", () => {
+    const result = sanitizeExp("`!model.isActive`");
+    expect(result.expression).toBe("`!context.model.isActive`");
+})
+
+test("sanitize - Not expressions in expressions", () => {
+    const result = sanitizeExp("!isActive && !isOn");
+    expect(result.expression).toBe("!context.isActive && !context.isOn");
+})
+
+test("sanitize - Not expression with prefix", () => {
+    const result = sanitizeExp("!$globals.isActive");
+    expect(result.expression).toBe("!crsbinding.data.globals.isActive");
+})
+
+test("sanitize - $context equals expression", () => {
+    const result = sanitizeExp("$context.value1 == $context.value2");
+    expect(result.expression).toBe("context.value1 == context.value2");
+})
+
+test("sanitize - equals expression using '=='", () => {
+    const result = sanitizeExp("value1 == value2");
+    expect(result.expression).toBe("context.value1 == context.value2");
+})
+
+test("sanitize - equals expression using '==='", () => {
+    const result = sanitizeExp("value1 === value2");
+    expect(result.expression).toBe("context.value1 === context.value2");
+})
+ */
