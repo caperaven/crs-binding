266c5b9025049a49c3c3ee2f116c86637d65de27
caperaven <caperaven@gmail.com>
2022-08-30 16:59:28 +0200

    add testing for expressions

--- app/form/form.js
+++ app/form/form.js
@@ -2,4 +2,11 @@ export default class Form extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
     }
+
+    preLoad() {
+        crsbinding.translations.add({
+            "firstName": "First Name",
+            "lastName": "Last Name"
+        }, "form")
+    }
 }
