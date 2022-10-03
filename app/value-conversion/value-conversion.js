4edde7b400de1ba72389c9e9e3bbe597e9f429e9
caperaven <caperaven@gmail.com>
2022-10-03 12:06:06 +0200

    add inflation manager feature for value conversion

--- app/value-conversion/value-conversion.js
+++ app/value-conversion/value-conversion.js
@@ -3,13 +3,30 @@ export default class ValueConversion extends crsbinding.classes.ViewBase {
         await super.connectedCallback();
     }
 
+    async disconnectedCallback() {
+        crsbinding.valueConvertersManager.remove("ascii");
+        await crsbinding.inflationManager.unregister("display");
+        await super.disconnectedCallback();
+    }
+
     preLoad() {
         crsbinding.valueConvertersManager.add("ascii", asciiConverter);
-
-        const result = crsbinding.valueConvertersManager.convert(65, "ascii", "get");
-        console.log(result);
-
         this.setProperty("char", 65);
+    }
+
+    load() {
+        const template = this._element.querySelector("template");
+        crsbinding.inflationManager.register("display", template);
+        super.load();
+    }
+
+    async charChanged(newValue) {
+        const element = this._element.querySelector("#display");
+        const fragment = crsbinding.inflationManager.get("display", { char: newValue }, element);
+
+        if (fragment != null) {
+            this._element.appendChild(fragment)
+        }
     }
 }
 
